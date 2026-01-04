c0109affa8d2ba6f8f61873c19704d1b24f8b3fa
Valeferr <valentinaferrentino2209@gmail.com>
2026-01-04 16:22:16 +0100

    Added frontend

---
+++ frontend/trasite/lib/api_service.dart
@@ -0,0 +1,43 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:latlong2/latlong.dart';
+import 'airbnb_listing.dart';
+
+class ApiService {
+  final Dio _dio = Dio();
+
+  String get baseUrl {
+    if (kIsWeb) {
+      return "http://localhost:8000"; // Web
+    } else if (defaultTargetPlatform == TargetPlatform.android) {
+      return "http://10.0.2.2:8000"; // Emulatore Android
+    } else {
+      return "http://127.0.0.1:8000"; // iOS o Desktop
+    }
+  }
+
+  Future<List<AirbnbListing>> fetchListingsInArea(LatLng center, {double delta = 0.05}) async {
+    try {
+      final response = await _dio.get(
+        '$baseUrl/distance/', 
+        queryParameters: {
+          'lat': center.latitude,
+          'lon': center.longitude,
+          'delta': delta,
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final data = response.data;
+        final List<dynamic> results = data['results'];
+
+        return results.map((json) => AirbnbListing.fromJson(json)).toList();
+      } else {
+        throw Exception("Errore backend: ${response.statusCode}");
+      }
+    } catch (e) {
+      print("Errore durante il fetch: $e");
+      return [];
+    }
+  }
+}
---
+++ frontend/trasite/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'airbnb_map_screen.dart'; 
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Airbnb Trust Map',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const AirbnbMapScreen(),
+    );
+  }
+}
